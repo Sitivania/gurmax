feee73911c9e09eb32e69ec4bc1bb6bf06fab4b7
Sitivania <103745666+Sitivania@users.noreply.github.com>
2025-08-30 14:51:32 +0700

    Update Header.vue

--- app/components/layouts/Header.vue
+++ app/components/layouts/Header.vue
@@ -65,7 +65,7 @@ const { title, github } = useAppConfig()
                 href="/dashboard"
                 :title="`${title} Dashboard`"
               >{{ $t('dashboard.title') }}</a>
-              <a
+              <!-- <a
                 :href="github"
                 target="_blank"
                 title="Github"
@@ -74,7 +74,7 @@ const { title, github } = useAppConfig()
                 <GitHubIcon
                   class="w-5 h-5 mr-1"
                 />
-                GitHub</a>
+                GitHub</a> -->
 
               <SwitchLanguage />
 
